fbc7c23709ffe29fcb5d8196c3067c90dee37324
sakshi <sakshi68@github.com>
2023-12-02 23:56:21 +0530

    Initial commit

--- app.js
+++ app.js
@@ -1,14 +1,58 @@
-const http = require('http')
+ const {readFile, writeFile} = require('fs').promises
 
-const server = http.createServer((req,res)=>{
-  if(req.url === '/'){
-    res.write('Welcome to our home page')
-    res.end()
+//  const util = require('util')
+ 
+//  const readFilePromise = util.promisify(readFile)
+//  const writeFilePromise = util.promisify(writeFile)
+
+
+ const start= async()=>{
+  try{
+    const first = await readFile('./content/first.txt');
+    const second = await readFile('./content/second.txt');
+    const result = await writeFile('./content/await-fianl.txt',first + second+'this ends here.',);
+    console.log('done')
+  }catch (error) {
+    console.log(error)
   }
-  res.end(
-     `oops!! no page found..`
-      )
+   
+}
+ start();
   
-})
-
-server.listen(5000);
+ console.log('hi');
+//  const start= async()=>{
+//   try{
+//     const first = await readFilePromise('./content/first.txt');
+//     const second = await readFilePromise('./content/second.txt');
+//     const result = await writeFilePromise('./content/await-fianl.txt',first + second+'this ends here.',);
+//     console.log('done')
+//   }catch (error) {
+//     console.log(error)
+//   }
+   
+// }
+ 
+//  const getText = (path)=>{
+  //   return new Promise((resolve,reject)=>{
+  //     readFile( path,'utf8',(err, result)=>{
+  //       if(err){
+  //          reject(err)
+  //       }else{
+  //         resolve(result)
+  //       }
+  //      })
+  //   })
+  //  }
+  
+  //  const write = (path)=>{
+  //   return new Promise((resolve,reject)=>{
+  //     writeFile('./content/await.txt',path,(err,result)=>{
+  //       if(err){
+  //         reject(err)
+  //       }else{
+  //         resolve(result)
+  //       }
+  //     })
+  //   })
+  //  }
+  
